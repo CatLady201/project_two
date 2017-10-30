befb0b9859a2b37c288d53d7fb72e01fd5721a9d
John Mikkola <john.mikkola@gmail.com>
2017-10-30 18:58:15 -0400

    Created visual studio project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project_Two {
+
+  class Program {
+
+    static void Main(string[] args) {
+      Console.WriteLine("Project Two Main Function");
+
+      Console.ReadKey();
+    }
+
+  }
+}
